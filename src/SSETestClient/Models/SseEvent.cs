3a90ac39256e84c9cdc7b28a71565a26a5c82971
Nikita Tulenev <n1tulenev@gmail.com>
2024-05-11 22:01:22 +0200

    Add SseEvent model

---
+++ src/SSETestClient/Models/SseEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace SSETestClient.Models;
+
+public class SseEvent
+{
+    public string Key { get; }
+    public string Payload { get; }
+
+    public SseEvent(string key, string payload)
+    {
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(key);
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(payload);
+
+        Key = key;
+        Payload = payload;
+    }
+
+    public override string ToString() => $"{Key}: {Payload}";
+}
