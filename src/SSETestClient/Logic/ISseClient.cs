d19e050231f19d3d929a40f757ed120313c66b4c
Nikita Tulenev <n1tulenev@gmail.com>
2024-05-12 07:45:39 +0200

    Add xmldoc for ISseClient

--- src/SSETestClient/Logic/ISseClient.cs
+++ src/SSETestClient/Logic/ISseClient.cs
@@ -2,7 +2,20 @@
 
 namespace SSETestClient.Logic;
 
+/// <summary>
+/// Defines a client interface for connecting 
+/// to a server and receiving SSE (Server-Sent Events).
+/// </summary>
 public interface ISseClient
 {
+    /// <summary>
+    /// Asynchronously connects to a server using 
+    /// the specified URI and listens for SSE events.
+    /// </summary>
+    /// <param name="uri">The URI of the server to connect to.</param>
+    /// <param name="cancellationToken">A token to monitor for 
+    /// cancellation requests.</param>
+    /// <returns>A stream of <see cref="SseEvent"/> instances that can be 
+    /// asynchronously enumerated.</returns>
     IAsyncEnumerable<SseEvent> ConnectAsync(Uri uri, CancellationToken cancellationToken);
 }
